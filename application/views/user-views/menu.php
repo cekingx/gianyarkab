866d14f8f14316b6897242d378e8015f5a5b63db
Dirga Yasa <cekingx@gmail.com>
2020-09-12 08:57:38 +0800

    Minor fix

--- application/views/user-views/menu.php
+++ application/views/user-views/menu.php
@@ -199,7 +199,7 @@
 
 	<!-- Scripts -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url('menus/collapse.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/menus/collapse.js') ?>"></script>
 	<script type="text/javascript">
 		$('.menubar.parent').click(function () {
 
